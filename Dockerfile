fbdbfe6e94db2da6951a6c7301f4351c631c4745
ce-a11y <ce339517@gmail.com>
2025-02-23 18:04:20 -0300

    Inserção de camada de proteção: requer seja enviado o parâmetro 'authorization' no header para prosseguimento do código.

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,10 @@ FROM node:22
 WORKDIR /app
 
 COPY ./package.json ./package-lock.json ./
-RUN npm install
+RUN npm install && npm install -g nodemon
 
 COPY . .
 
 EXPOSE 3000
 
-CMD ["node", "server.js"]
+CMD ["nodemon", "--legacy-watch", "--inspec=0.0.0.0", "server.js"]
--- server.js
+++ server.js
@@ -7,13 +7,30 @@ import { buscaCondominio } from './src/buscaCondominio.js';
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    try {
+        const apiKey = req.headers['authorization'];
+
+        if (!apiKey || apiKey !== process.env.SECRET_API_KEY) {
+           return res.status(403).json({mensagem: "Erro: acesso negado. Chave de API inválida."});
+        };
+
+        next();
+    } catch (error) {
+        console.error("Erro na autenticação", error);
+        res.status(500).json({mensagem: "Erro interno"});
+    }
+    })
 
 app.post('/busca-condominio', async (req, res) => {
     const {nomeCondominio} = req.body;
-    const resposta = await buscaCondominio(nomeCondominio)
+    const resposta = await buscaCondominio(nomeCondominio);
     return res.json(resposta);
-})
+});
 
 app.post('/busca-unidades', async (req, res) => {
 
@@ -21,22 +38,27 @@ app.post('/busca-unidades', async (req, res) => {
     const resposta = await buscarUnidades(idCondominio, cpf);
     return res.json(resposta);
 
-})
+});
 
 app.post('/webhook-boleto', async (req, res) => {
 
     const { idCondominio, idUnidade } = req.body;
     const resposta = await buscarBoleto( idCondominio, idUnidade );
-    return res.json(resposta)
+    return res.json(resposta);
 
 });
 
 app.get('/', (req, res) => {
 
-    res.send("Webhook rodadno na porta 3000")
+    res.send("Webhook rodando na porta 3000");
 
-})
+});
+
+app.use((err, req, res, next) => {
+    console.error("Erro inesperado:", err);
+    res.status(500).json({ erro: "Erro interno no servidor" });
+});
 
 app.listen(3000, () => {
-    console.log("Webhook rodando na porta 3000")
-})
+    console.log("Webhook rodando na porta 3000");
+});
